9b57b870dcb95c12abe35e71dbf4b445501806ee
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-03-30 00:42:53 -0700

    Completed issue-1-create-an-entry.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 /* global data */
 /* exported data */
-
-var $photoUrl = document.getElementById('photo-url');
-var $photoPreview = document.getElementById('photo-preview');
+var $photoUrl = document.getElementById('photoUrl');
+var $photoPreview = document.getElementById('photoPreview');
 var $entryForm = document.getElementById('entry-form');
 
 $photoUrl.addEventListener('input', function (event) {
   $photoPreview.src = event.target.value;
-  // console.log($photoUrl, event)
 });
 
 $entryForm.addEventListener('submit', function (event) {
-  //  var formInputUpdated = event.target.value;
-  // formInputUpdated.nextEntryId =
+  event.preventDefault();
+  var formInputValue = {
+    entryId: data.nextEntryId++,
+    notes: event.target.elements.notes.value,
+    photoURL: event.target.elements.photoUrl.value,
+    title: event.target.elements.title.value
+  };
+  data.entries.unshift(formInputValue);
+  $photoPreview.src = './images/placeholder-image-square.jpg';
+  $entryForm.reset();
+});
+
+window.addEventListener('beforeunload', function () {
+  var formStorage = JSON.stringify(data);
+  localStorage.setItem('code-journal-input', formStorage);
 });
