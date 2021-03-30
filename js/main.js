6a62a35bed3a0612b951416121d0e199b385f508
Sarah Ryu Ocampo <socampo47@gmail.com>
2021-03-29 23:09:34 -0700

    Halfway issue-1-create-an-entry.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.getElementById('photo-url');
+var $photoPreview = document.getElementById('photo-preview');
+var $entryForm = document.getElementById('entry-form');
+
+$photoUrl.addEventListener('input', function (event) {
+  $photoPreview.src = event.target.value;
+  // console.log($photoUrl, event)
+});
+
+$entryForm.addEventListener('submit', function (event) {
+  //  var formInputUpdated = event.target.value;
+  // formInputUpdated.nextEntryId =
+});
